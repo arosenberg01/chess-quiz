b8bc3f4cb74b19c2db634db67bb528bdf1117c37
arosenberg01 <ansel01@gmail.com>
2014-12-14 22:37:07 -0800

    All basic features included

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 $(document).ready(function() {
-	var questionNum = 0;
-	var numCorrect = 0;
+	var questionNum, numCorrect;
 
 	var questions = [  
 		{question: "Chess is often called the \'game of...\'", 
-		 	answers: ["queens", "kings", "tactics", "strategy"] },   
+		 	answers: ["Queens", "Kings", "Tactics", "Strategy"] },   
 		{question: "Who is the current World Champion in chess?", 
 			answers: ["Magnus Carlsen", "Gary Kasparov", "Viswanathan Anand", "Bobby Fischer"] },	
 		{question: "The most famous American chess player of all time is generally considered to be...", answers: ["Samuel Reshevsky", "Hikaru Nakamura", "Bobby Fischer", "Paul Morphy"]},
 		{question: "A chess board consists of ___ squares.", answers: ["48", "72", "64", "49"]}
 		];
 
-	var correctAnswers = [undefined, "kings", "Magnus Carlsen", "Bobby Fischer", "64"];
+	var correctAnswers = ["Kings", "Magnus Carlsen", "Bobby Fischer", "64"];
 
-	nextQuestion();
+	$('#startQuiz').on('click', function(e) {
+		e.preventDefault();
+		$('#startQuiz').hide();
+		start();
+	});
 
 	$('#submitButton').on('click', function(e) {
 		e.preventDefault();
-		if (questionNum < 4)	{
-			nextQuestion();
-		} else {
-			$('#feedback h2').text(numCorrect - 1 + " out of 4");
-		}
+		nextQuestion();
 	});  
 		
+	function start() {
+		questionNum = 0;
+		numCorrect = 0;
+		$('#currentQ').show();
+		$('#answers').show();
+		$('#submitButton').css('display', 'block')
 
+		$('#currentQ').html(questions[questionNum].question)
+
+		for (var k = 0; k < 4; k++) {
+			$("label[for='" + String.fromCharCode(97 + k) + "']").text(questions[questionNum].answers[0 + k]);
+		}
+	}
 		
 	function nextQuestion() {
 
@@ -34,35 +45,36 @@ $(document).ready(function() {
 		if (submittedAnswer === correctAnswers[questionNum]) {
 			numCorrect++;
 			$('#feedback h2').text("Correct");
+			$('#answers').animate()
 		} else {
 			$('#feedback h2').text("Incorrect");
 		}
 
-		// submittedAnswer === correctAnswers[questionNum] ? console.log("Correct") : console.log("Incorrect");
-
-		// console.log(submittedAnswer + "----" + correctAnswers[questionNum]);
+		console.log(submittedAnswer + "----" + correctAnswers[questionNum] + "----" + numCorrect);
 
 		$('input[type="radio"]').prop('checked', false);
 
-		$('#currentQ').html(questions[questionNum].question)
+		questionNum += 1;
+		
+		if (questionNum < 4)	{
+			$('#currentQ').html(questions[questionNum].question)
 
-		for (var i = 0; i < 4; i++) {
-			$("label[for='" + String.fromCharCode(97 + i) + "']").html(questions[questionNum].answers[0 + i]);
+			for (var i = 0; i < 4; i++) {
+			$("label[for='" + String.fromCharCode(97 + i) + "']").text(questions[questionNum].answers[0 + i]);
+			}
+
+		} else {
+			$('#feedback h2').text(numCorrect + " out of 4");
+			$('#currentQ').hide();
+			$('#answers').hide();
+			$('#submitButton').hide();
+			$('#startQuiz').show();
 		}
 
-		questionNum += 1;
+
 
 	}
 
 	
 
-
-
-
-
-
-
-	
-
 });
-
